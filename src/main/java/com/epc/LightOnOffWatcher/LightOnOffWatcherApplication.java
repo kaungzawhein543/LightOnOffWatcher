d8d1decd6a1a993820d0efd8fa635f69ffe1cae9
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-01-12 08:00:09 +0630

    first commit

---
+++ src/main/java/com/epc/LightOnOffWatcher/LightOnOffWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.epc.LightOnOffWatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightOnOffWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightOnOffWatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epc/LightOnOffWatcher/LightOnOffWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epc.LightOnOffWatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightOnOffWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
