2f2c3554b5b60b77d8c77510e330a5dffc07c176
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-01-12 10:31:18 +0630

    UI Error solving

--- src/main/java/com/epc/LightOnOffWatcher/controller/index.java
+++ src/main/java/com/epc/LightOnOffWatcher/controller/index.java
@@ -1,13 +1,26 @@
 package com.epc.LightOnOffWatcher.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class index {
 
+    private boolean group = true;
+
     @GetMapping("/")
-    public String indexRoute() {
+    public String indexRoute(Model m) {
+        m.addAttribute("group", group);
         return "index";
+    }
+
+    @PostMapping("/changeValue")
+    public String changeValue(Model m) {
+        this.group = !group;
+        m.addAttribute("group", group);
+        return "redirect:/";
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-  './src/**/*.{html.js}',
-  './templates/**/*.html',
-  ],
+    content: [
+     "./src/main/resources/templates/**/*.{html,js}",
+     "./src/main/resources/static/**/*.{html,js}"
+   ],
   theme: {
     extend: {},
   },
