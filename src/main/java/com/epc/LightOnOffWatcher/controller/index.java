bbcc89444c33dea8305c637e6ab3d14b34adbddd
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-01-13 20:38:33 +0630

    Version 1 Complete

--- src/main/java/com/epc/LightOnOffWatcher/controller/index.java
+++ src/main/java/com/epc/LightOnOffWatcher/controller/index.java
@@ -2,6 +2,7 @@ package com.epc.LightOnOffWatcher.controller;
 
 import com.epc.LightOnOffWatcher.CalculateTime;
 import com.epc.LightOnOffWatcher.bean.GroupAndDate;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Arrays;
 
 @Controller
 public class index {
@@ -20,14 +23,17 @@ public class index {
         if(m.containsAttribute("times")) {
             m.addAttribute("times", m.getAttribute("times"));
         }
+        m.addAttribute("inputData", new GroupAndDate());
         m.addAttribute("group", group);
         return "index";
     }
 
-    @PostMapping("/getTime")
-    public String getTime(@RequestBody GroupAndDate inputData, Model m, RedirectAttributes redirectAttributes){
+    @PostMapping(value="/getTime")
+    public String getTime(@ModelAttribute GroupAndDate inputData, RedirectAttributes redirectAttributes){
+        System.out.println(inputData);
         CalculateTime calculateTime = new CalculateTime();
         String[] timeArray =  calculateTime.calculate(Integer.parseInt(inputData.getDay()), inputData.getGroup());
+        System.out.println(Arrays.toString(timeArray));
         redirectAttributes.addFlashAttribute("times", timeArray);
         return "redirect:/";
     }
